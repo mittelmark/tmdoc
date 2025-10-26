2178eeb80b113d97fd34a93a8f510bd1786acd1a
Detlef Groth (D7480) <>
2025-10-26 15:53:13 +0000

    fixing PATH update for install-tmdoc.sh with question

--- install-tmdoc.sh
+++ install-tmdoc.sh
@@ -11,6 +11,30 @@ function installBinary() {
     chmod 755 ~/.local/bin/$APP
     ~/.local/bin/$APP --version
     echo "installed $APP"
+    if [[ "`echo $PATH | grep /.local/bin`" == "" ]]; then 
+        echo "~/.local/bin is not in your PATH"
+        echo "add the following line to your .bashrc or .zshrc file"
+        echo "echo \$PATH | grep -q /.local/bin || export PATH=~/.local/bin:\$PATH"
+        yes_or_no "Do you like to add this PATH adaptation to your .bashrc file?" && add_local_bin_path
+    else 
+        echo "~/.local/bin is already in your PATH"
+    fi 
+
+}
+
+function add_local_bin_path {
+    echo "echo \$PATH | grep -q /.local/bin || export PATH=~/.local/bin:\$PATH" >> ~/.bashrc
+    source ~/.bashrc    
+    echo "Path was updated in .bashrc"
+}
+function yes_or_no {
+   while true; do
+       read -p "$* [y/n]: " yn
+        case $yn in
+            [Yy]*) return 0  ;;  
+            [Nn]*) echo "Aborted" ; return  1 ;;
+        esac
+    done
 }
 
 if [[ ! -d ~/.local/bin ]]; then
